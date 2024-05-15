7f756cdcd48445306802bf802f13749918890e95
Guilherme Lazani Lopes <guilherme-lazarini@hotmail.com>
2024-05-15 15:41:14 -0300

    Created Category and product

---
+++ src/Store.Catalogo.Domain/Category.cs
@@ -0,0 +1,27 @@
+﻿using Store.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Catalogo.Domain
+{
+    public class Category : Entity
+    {
+        public string Name { get; private set; }
+        public int Code { get; private set; }
+
+        public Category(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
+    }
+}
+
---
+++ src/Store.Catalogo.Domain/Product.cs
@@ -0,0 +1,64 @@
+﻿using Store.Core.DomainObjects;
+
+namespace Store.Catalogo.Domain
+{
+    public class Product : Entity, IAggreagateRoot
+    {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime DateRegister { get; private set; }
+        public string Image {  get; private set; }
+        public int QuantityStock { get; private set; }
+
+        public Category Category { get; private set; }
+
+        public Product( string name, string description, bool active, decimal value, Guid categoryId, DateTime dateRegister, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Value = value;
+            DateRegister = dateRegister;
+            Image = image;
+        }
+
+        public void Activate() => Active = true;
+
+        public void Deactivate() => Active = false;
+
+        public void UpdateCategory(Category category) 
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void UpdateDesCription(string description) 
+        {  
+            Description = description; 
+        }
+
+        public void DebitStock(int quantity) 
+        {
+            if (quantity < 0) quantity *= -1;
+            QuantityStock -= quantity;
+        }
+
+        public bool ReplenishStock(int quantity)
+        {
+            return QuantityStock >= quantity;        
+        }
+
+        public bool HasStock(int quantity)
+        {  
+            return QuantityStock >= quantity; 
+        }
+
+        public void Validate()
+        {
+        }
+    }
+}
