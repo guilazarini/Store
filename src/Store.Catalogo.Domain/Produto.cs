b2a293851073343456c7986554890bf172029735
Guilherme Lazani Lopes <guilherme-lazarini@hotmail.com>
2024-05-15 15:01:24 -0300

    Add project files.

---
+++ src/Store.Catalogo.Domain/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Catalogo.Domain
+{
+    public class Produto
+    {
+      public Guid Id { get; set; }
+
+    }
+
+    public class Categoria 
+    { 
+        public Guid Id {  set; get; }
+
+    }
+}
