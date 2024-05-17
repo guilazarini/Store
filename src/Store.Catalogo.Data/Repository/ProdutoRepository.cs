a351ce10b7e5198f23b04dff0da7ac081e4f22b4
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-05-17 17:15:45 -0300

    Add-repository

---
+++ src/Store.Catalogo.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Catalogo.Domain;
+using Store.Core.Data;
+
+namespace Store.Catalogo.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _catalogoContext;
+    
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+
+        public IUnitOfWork UnitOfWork => _catalogoContext;
+
+        public void AddCategoria(Categoria categoria)
+        {
+            _catalogoContext.Categorias.Add(categoria);
+        }
+
+        public void AddProduto(Produto produto)
+        {
+            _catalogoContext.Produtos.Add(produto);
+        }
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Categoria>> GetAllCategorias()
+        {
+            return await _catalogoContext.Categorias.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> GetByIdCategoria(int codigo)
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().Include(p => p.Categoria).Where(c => c.Categoria.Codigo == codigo).ToListAsync();
+        }
+
+        public async Task<Produto> GetById(Guid id)
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public void UpdateCategoria(Categoria categoria)
+        {
+            _catalogoContext.Categorias.Update(categoria);
+        }
+
+        public void UpdateProduto(Produto produto)
+        {
+            _catalogoContext.Produtos.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+    }
+}
---
+++ src/Store.Catalogo.Domain/IProdutoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store.Core.Data;
+
+namespace Store.Catalogo.Domain
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> GetAll();
+        Task<Produto> GetById(Guid id);
+        Task<IEnumerable<Produto>> GetByIdCategoria(int codigo);
+        Task<IEnumerable<Categoria>> GetAllCategorias();
+
+        void AddProduto(Produto produto);
+        void UpdateProduto(Produto produto);
+
+        void AddCategoria(Categoria categoria);
+        void UpdateCategoria(Categoria categoria);
+    }
+}
