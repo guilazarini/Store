63d59709c3a2bd4f44d5f5c630e3d071f69558d6
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-05-26 16:56:44 -0300

    Add-WebApp

---
+++ src/Store.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Store.Catalogo.Application.Services;
+using Store.Catalogo.Data;
+using Store.Catalogo.Data.Repository;
+using Store.Catalogo.Domain;
+using Store.Catalogo.Domain.Interfaces;
+using Store.Core.Bus;
+
+namespace Store.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services) 
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+        }
+    }
+}
