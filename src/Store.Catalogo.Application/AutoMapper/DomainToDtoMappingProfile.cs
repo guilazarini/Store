ecb4c8108a1acee376129c14a9bf122a2f9ebc03
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-05-26 11:00:06 -0300

    Add-Produto-and-Categoria-MappingProfile

---
+++ src/Store.Catalogo.Application/AutoMapper/DomainToDtoMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Store.Catalogo.Application.DTOs;
+using Store.Catalogo.Domain;
+using System;
+using System.Collections.Generic;
+namespace Store.Catalogo.Application.AutoMapper
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<Produto, ProdutoDto>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaDto>();
+        }
+    }
+}
---
+++ src/Store.Catalogo.Application/AutoMapper/DtoToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Store.Catalogo.Application.DTOs;
+using Store.Catalogo.Domain;
+
+namespace Store.Catalogo.Application.AutoMapper
+{
+    public class DtoToDomainMappingProfile : Profile 
+    {
+        public DtoToDomainMappingProfile()
+        {
+            CreateMap<ProdutoDto, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaDto, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
