e0819354df8d4706cad051c4c217a6268e187a98
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-06-03 22:35:55 -0300

    Add-Domain-Objects

--- Store.Vendar.Domain/PedidoStatus.cs
+++ Store.Vendar.Domain/PedidoStatus.cs
@@ -1,8 +1,12 @@
 ﻿namespace Store.Vendar.Domain
 {
     public enum PedidoStatus
-    { 
-    
+    {
+        Rascunho = 0,
+        Iniciado = 1,
+        Pago = 4,
+        Entregue = 5,
+        Cancelado = 6
     }
 
 }
