7cef2196bf4872b50df061310e0b6ac0e6aa59a9
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-06-10 13:12:54 -0300

    Add-Dtos-Queries

---
+++ src/Store.Vendas.Application/Queries/DTOs/PedidoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Vendas.Application.Queries.DTOs
+{
+    public class PedidoDto
+    {
+        public int Codigo { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int PedidoStatus { get; set; }
+    }
+}
---
+++ src/Store.Vendas.Application/Queries/IPedidoQueries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Vendas.Application.Queries
+{
+    public interface IPedidoQueries
+    {
+    }
+}
