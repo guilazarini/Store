d028a46c82bac1518a591434331fcf22b860996a
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-06-10 21:59:04 -0300

    Adds-Events

---
+++ src/Store.Vendas.Application/Events/VoucherAplicadoPedidoEvent.cs
@@ -0,0 +1,20 @@
+﻿using Store.Core.Messages;
+
+
+namespace Store.Vendas.Application.Events
+{
+    public class VoucherAplicadoPedidoEvent : Event
+    {
+        public Guid ClienteId { get; private set; }
+        public Guid PedidoId { get; private set; }
+        public Guid VoucherId { get; private set; }
+
+        public VoucherAplicadoPedidoEvent(Guid clienteId, Guid pedidoId, Guid voucherId)
+        {
+            AggregateID = pedidoId;
+            ClienteId = clienteId;
+            PedidoId = pedidoId;
+            VoucherId = voucherId;
+        }
+    }
+}
