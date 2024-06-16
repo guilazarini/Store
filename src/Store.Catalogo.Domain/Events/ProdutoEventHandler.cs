47c7342149059313aac4c0db9f6e6431a9644391
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-06-16 20:15:18 -0300

    Add-Event

--- src/Store.Catalogo.Domain/Events/ProdutoEventHandler.cs
+++ src/Store.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -1,21 +1,44 @@
 ﻿using MediatR;
 using Store.Catalogo.Domain.Interfaces;
+using Store.Core.Communication.Mediator;
+using Store.Vendas.Application.Events;
 
 namespace Store.Catalogo.Domain.Events
 {
-    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    public class ProdutoEventHandler : 
+        INotificationHandler<ProdutoAbaixoEstoqueEvent>,
+        INotificationHandler<PedidoIniciadoEvent>
+                                        
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IEstoqueService _estoqueService;
+        private readonly IMediatorHandler _mediatorHandler;
 
-        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        public ProdutoEventHandler(IProdutoRepository produtoRepository, IEstoqueService estoqueService, IMediatorHandler mediatorHandler)
         {
             _produtoRepository = produtoRepository;
+            _estoqueService = estoqueService;
+            _mediatorHandler = mediatorHandler;
         }
 
         public async Task Handle(ProdutoAbaixoEstoqueEvent notification, CancellationToken cancellationToken)
         {
             var produto = await _produtoRepository.GetById(notification.AggregateID);
 
+        }
+
+        public async Task Handle(PedidoIniciadoEvent notification, CancellationToken cancellationToken)
+        {
+            var result = await _estoqueService.DebitarListaProdutosPedido(notification.ProdutosPedidos);
+
+            if (result)
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueConfirmadoEvent(notification.PedidoID, notification.ClienteId, notification.Total, notification.ListaProdutosPedido, notification.NomeCartao, notification.NumeroCartao,notification.ExpiracaoCartao, notification.CvvCartao));
+            }
+            else
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueRejeitadoEvent(notification.PedidoID, notification.ClienteId));
+            }
         }
     }
 }
