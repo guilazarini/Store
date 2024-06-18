6f6f0768b71dbe379d5b31cadbfc0eefd631b860
Guilherme Lazani Lopes <guilhermelazarinilopes@gmail.com>
2024-06-17 22:54:42 -0300

    add-EventProcessamentoCancelado

---
+++ Store.Core/Messages/IntagrationEvents/PedidoProcessamentoCanceladoEvent.cs
@@ -0,0 +1,19 @@
+﻿using Store.Core.DomainObjects.Dtos;
+
+namespace Store.Core.Messages.IntagrationEvents
+{
+    public class PedidoProcessamentoCanceladoEvent : IntegrationEvent
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public ListaProdutosPedido ProdutosPedido { get; private set; }
+
+        public PedidoProcessamentoCanceladoEvent(Guid pedidoId, Guid clienteId, ListaProdutosPedido produtosPedido)
+        {
+            AggregateID = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            ProdutosPedido = produtosPedido;
+        }
+    }
+}
